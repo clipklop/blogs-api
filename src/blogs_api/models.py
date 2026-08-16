9331eac6c4e8eb520857892537a1dc73a5a94c3c
clipklop <siliconchik@gmail.com>
2026-08-16 12:30:54 +0300

    Added user model

--- src/blogs_api/models.py
+++ src/blogs_api/models.py
@@ -23,3 +23,21 @@ class Post(Base):
         server_default=func.now(), 
         onupdate=func.now(),
     )
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    username: Mapped[str] = mapped_column(String(100), unique=True, nullable=False)
+    email: Mapped[str] = mapped_column(String(255), unique=True, nullable=False)
+    password: Mapped[str] = mapped_column(String(255), nullable=False)
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+    )
+    updated_atL: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+        onupdate=func.now(),
+    )
