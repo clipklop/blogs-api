d99b561c371484039c0b852c7749884b5380c96b
clipklop <siliconchik@gmail.com>
2026-09-02 23:58:52 +0300

    Added Oauth module

---
+++ src/blogs_api/oauth2.py
@@ -0,0 +1,6 @@
+import jwt
+
+SECRET_KEY = "1e161ee6687d59903b9c7776c7c23e6d2a9f47a93b25ef447b733e456230a66e"  # Replace with your actual secret key, like with openssl rand -hex 32
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
