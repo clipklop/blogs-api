554162685883b64b4f1048236c3df6c360e0e04c
clipklop <siliconchik@gmail.com>
2026-09-03 00:15:33 +0300

    Added Oauth module

--- src/blogs_api/oauth2.py
+++ src/blogs_api/oauth2.py
@@ -1,6 +1,15 @@
+from datetime import datetime, timedelta
+
 import jwt
 
 SECRET_KEY = "1e161ee6687d59903b9c7776c7c23e6d2a9f47a93b25ef447b733e456230a66e"  # Replace with your actual secret key, like with openssl rand -hex 32
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.now() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+    return encoded_jwt
--- src/blogs_api/routers/auth.py
+++ src/blogs_api/routers/auth.py
@@ -5,6 +5,7 @@ from blogs_api import models
 from blogs_api.database import get_db
 from blogs_api.schemas import UserLogin
 from blogs_api.utils import verify_password
+from blogs_api.oauth2 import create_access_token
 
 router = APIRouter(tags=["Authentication"])
 
@@ -19,5 +20,6 @@ def login(user_login: UserLogin, db: Session = Depends(get_db)):
     if not verify_password(user_login.password, user.password):
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid email or password")
 
-    # TODO: Implement token generation and return the token instead of a success message
-    return {"token": "Token generation not implemented yet", "message": "Login successful"}
+    access_token = create_access_token(data={"user_id": user.id})
+
+    return {"token_type": "bearer", "access_token": access_token}
